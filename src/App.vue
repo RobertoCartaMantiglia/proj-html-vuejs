179e49fdb529e8c961beaced34bb8fa59d8201ac
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-18 15:35:18 +0100

    start job and done scaffholding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import CompHeader from './components/CompHeader.vue';
+
+export default {
+
+  components: {
+    CompHeader,
+  }
+}
+</script>
+
+<template>
+
+  <CompHeader />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+</style>
---
+++ src/components/CompHeader.vue
@@ -0,0 +1,15 @@
+
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div>
+        <h1 class="text-warning">Project music</h1>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
