fd40f56e3451c9fb39842550d0d20eeeddb8592b
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-19 11:17:50 +0100

    improve main section layout

--- src/components/CompMain.vue
+++ src/components/CompMain.vue
@@ -8,14 +8,13 @@ export default {
         <section class="first-section">
             <div class="container">
                 <div class="row">
-                    <div class="col-8 m-auto pt-3">
+                    <div class="col-6 m-auto pt-3">
                         <h2 class="paragraph-title pb-4">Music is LIfe</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quia voluptates
                             laudantium,
                             dignissimos unde nobis adipisci illum pariatur provident neque nam, iure accusamus
                             ullam.
-                            Veniam
-                            sed quod aliquid sunt consequuntur.
+
                         </p>
                     </div>
                 </div>
@@ -24,17 +23,43 @@ export default {
         <section class="main-section">
             <div class="container">
                 <div class="row">
-                    <div class="col-8 m-auto pt-3">
+                    <div class="col-6 m-auto pt-3">
                         <h2 class="paragraph-title pb-4">Latest Band News</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quia voluptates
                             laudantium,
                             dignissimos unde nobis adipisci illum pariatur provident neque nam, iure accusamus
                             ullam.
-                            Veniam sed quod aliquid sunt consequuntur.
+
                         </p>
                     </div>
-                    <div class="col-8">
+                    <div class="row">
+                        <div class="col-10 d-flex m-auto">
+                            <div class="col-6 d-flex flex-wrap left-side">
+                                <div class="card horizontal-card">
 
+                                </div>
+
+                                <div class="card vertical-card col-6">
+
+                                </div>
+
+                                <div class="card vertical-card col-6">
+
+                                </div>
+                                <div class="card horizontal-card col-12">
+
+                                </div>
+
+                            </div>
+                            <div class="col-4 right-side">
+                                <div class="col-3 card vertical-card">
+
+                                </div>
+                                <div class="col-3 card vertical-card">
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -56,7 +81,18 @@ export default {
 }
 
 .main-section {
-    height: 700px;
     background-color: $bg-principal-section;
+}
+
+.horizontal-card {
+    width: 100%;
+    height: 180px;
+    background-color: $bg-color-small-section;
+}
+
+.vertical-card {
+    height: 300px;
+    width: 100%;
+    background-color: $bg-color-small-section;
 }
 </style>
