5552ec14e7464cd05a586b22b2727858becd3eba
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-19 10:09:47 +0100

    main section

--- src/components/CompMain.vue
+++ src/components/CompMain.vue
@@ -9,7 +9,7 @@ export default {
             <div class="container">
                 <div class="row">
                     <div class="col-8 m-auto pt-3">
-                        <h2 class="paragraph-title mb-5">Music is LIfe</h2>
+                        <h2 class="paragraph-title pb-4">Music is LIfe</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quia voluptates
                             laudantium,
                             dignissimos unde nobis adipisci illum pariatur provident neque nam, iure accusamus
@@ -17,6 +17,24 @@ export default {
                             Veniam
                             sed quod aliquid sunt consequuntur.
                         </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="main-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 m-auto pt-3">
+                        <h2 class="paragraph-title pb-4">Latest Band News</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quia voluptates
+                            laudantium,
+                            dignissimos unde nobis adipisci illum pariatur provident neque nam, iure accusamus
+                            ullam.
+                            Veniam sed quod aliquid sunt consequuntur.
+                        </p>
+                    </div>
+                    <div class="col-8">
+
                     </div>
                 </div>
             </div>
@@ -35,5 +53,10 @@ export default {
 
 .paragraph-title {
     border-bottom: 1px solid $words;
+}
+
+.main-section {
+    height: 700px;
+    background-color: $bg-principal-section;
 }
 </style>
